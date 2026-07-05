7b0be996c25329502f166a70576c85b298bf171a
Blankx <briceguichou.dev@gmail.com>
2026-07-05 08:15:50 +0200

    feat : creation of my_var() to print variable type

---
+++ Django_0_starting/ex00/var.py
@@ -0,0 +1,25 @@
+
+
+
+def my_var():
+    int_num = 42
+    str_num = "42"
+    string = "quatante deux"
+    float_num = 42.0
+    list_var = [42]
+    dict_var = {"42": "42"}
+    tuple_var = (42)
+    set_var = {42}
+    bool_var = True
+    print(f"Integer: {int_num} as type {type(int_num)}")
+    print(f"String: {str_num} as type {type(str_num)}")
+    print(f"String: {string} as type {type(string)}")
+    print(f"Float: {float_num} as type {type(float_num)}")
+    print(f"List: {list_var} as type {type(list_var)}")
+    print(f"Dictionary: {dict_var} as type {type(dict_var)}")
+    print(f"Tuple: {tuple_var} as type {type(tuple_var)}")
+    print(f"Set: {set_var} as type {type(set_var)}")
+    print(f"Boolean: {bool_var} as type {type(bool_var)}")
+
+if __name__ == "__main__":
+    my_var()
