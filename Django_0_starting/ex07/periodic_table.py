2c1062310d98360d8ed4bfb792f497c51fcb1ea3
Blankx <briceguichou.dev@gmail.com>
2026-07-06 23:00:35 +0200

    feat : create_nested_dict_from_list function to improve readability and structure
    the structure is {elements_name: {attribute1 : value1},{...}}

--- Django_0_starting/ex07/periodic_table.py
+++ Django_0_starting/ex07/periodic_table.py
@@ -12,10 +12,25 @@ def check_arguments():
 def parse_data_from_file(file_path):
     with open(file_path, 'r') as file:
         return file.read().strip().replace(",", "").splitlines()
+    
+def create_nested_dict_from_list(list):
+    elements = {}
+    for line in list:
+        element_name, data = line.split('=')
+        element_name = element_name.strip()
+        data_cleaned = data.replace(': ', ':')
+        internal_dict = {}
+        for block in data_cleaned.split():
+            if ':' in block:
+                key, value = block.split(':')
+                internal_dict[key] = value
+        elements[element_name] = internal_dict
+    return elements
 
 def main():
     check_arguments()
     list_lines = parse_data_from_file(argv[1])
+    dict_elements = create_nested_dict_from_list(list_lines)
 
 
 if __name__ == "__main__":
