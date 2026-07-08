c680458332ddbd7cdf3381b109fcd847ff381c59
Blankx <briceguichou.dev@gmail.com>
2026-07-08 13:21:53 +0200

    feat: enhance HTML rendering for periodic table with proper table structure and spacing

--- Django_0_starting/ex07/periodic_table.py
+++ Django_0_starting/ex07/periodic_table.py
@@ -30,16 +30,41 @@ def create_nested_dict_from_list(list):
 def get_html_boilerplate():
     html_template = """<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Periodic Table</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Periodic Table</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
     <table>
-        <tr>
-"""
+        <tr>\n"""
+    return html_template
+
+def render_periodic_table(elements):
+    html_template = get_html_boilerplate()
+    column = 0
+    buffer = ""
+    for name, info in elements.items():
+        while int(info["position"]) > column:
+            buffer += "\t\t<td></td>\n"
+            column += 1
+        buffer += "\t\t<td>\n"
+        buffer += f"\t\t\t<h4>{name}</h4>\n"
+        buffer += "\t\t\t<ul>\n"
+
+        buffer += f"\t\t\t\t<li>No: {info["number"]}</li>\n"
+        buffer += f"\t\t\t\t<li>{info["small"]}</li>\n"
+        buffer += f"\t\t\t\t<li>{info["molar"]}</li>\n"
+        buffer += f"\t\t\t\t<li>{info["electron"]}</li>\n"
+        buffer += "\t\t\t</ul>\n"
+        buffer += "\t\t</td>\n"
+        column += 1
+        if column == 18:
+            buffer += "\t\t</tr>\n\t\t<tr>\n"
+            column = 0
+        html_template += buffer
+        buffer = ""
     return html_template
 
 
@@ -47,6 +72,8 @@ def main():
     check_arguments()
     list_lines = parse_data_from_file(argv[1])
     dict_elements = create_nested_dict_from_list(list_lines)
+    html = render_periodic_table(dict_elements)
+    print(html)
 
 
 if __name__ == "__main__":
