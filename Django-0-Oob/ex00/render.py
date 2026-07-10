82ce027eb1efcb737be5a2648a256ea5481b6fc2
Blankx <briceguichou.dev@gmail.com>
2026-07-11 01:05:58 +0200

    feat: implement template processing and HTML generation
    -reading template file
    - get vars in dict from settings.py
    - usage of function format() for replace in template buffer
    - generating new html file base on file_name.template

--- Django-0-Oob/ex00/render.py
+++ Django-0-Oob/ex00/render.py
@@ -1,5 +1,5 @@
 from sys import argv, exit, stderr
-from os import rename
+from re import search
 
 
 def check_arguments():
@@ -14,13 +14,43 @@ def check_arguments():
 def read_template_file():
     try:
         with open(argv[1], 'r', encoding="utf-8") as file:
-            print(file.read())
+            return file.read()
     except FileNotFoundError:
         stderr.write("Error: File not found\n")
+        exit(1)
         
+def get_vars_in_dict():
+    try:
+        import settings
+        return {k: v for k, v in vars(settings).items() if not k.startswith("__")}
+    except ImportError:
+        stderr.write("Error: settings.py not found\n")
+        exit(1)
+def subsitute_var_in_html(template_content, vars_dict):
+    try:
+        html_content = template_content.format(**vars_dict)
+        return html_content
+    except KeyError as e:
+        stderr.write(f"Error: Variable {e} required by template is missing in settings.py\n")
+        exit(1)
+
+def generate_html_file(html_content):
+    output_file = argv[1].replace(".template", ".html")
+    try:
+        with open(output_file, "w", encoding="utf-8") as file:
+            file.write(html_content)
+    except Exception:
+        stderr.write("Error: Could not write output file\n")
+        exit(1)
+
+
 def main():
     check_arguments()
-    read_template_file()
+    template_content = read_template_file()
+    vars_dict = get_vars_in_dict()
+    html_content = subsitute_var_in_html(template_content, vars_dict)
+    generate_html_file(html_content)
 
+    
 if __name__ == "__main__":
     main()
