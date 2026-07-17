e1ef51a1346984fbc569e77725eb38a808a78871
Blankx <briceguichou.dev@gmail.com>
2026-07-17 14:18:32 +0200

    refacto: correct installation script messages to English and remove duplicate shebang

--- Django-1-lib/ex01/install_path.sh
+++ Django-1-lib/ex01/install_path.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#!/bin/sh
-
 pip3 --version
 
 LOG_FILE="install_path.log"
@@ -9,9 +7,9 @@ LOG_FILE="install_path.log"
 pip3 install --target=./local_lib -U git+https://github.com/jaraco/path.git > "$LOG_FILE" 2>&1
 
 if [ $? -eq 0 ]; then
-    echo "Installation réussie ! Lancement de mon_programme..."
+    echo "Installation success ! Starting program..."
     python3 my_program.py
 else
-    echo "Erreur lors de l'installation de path.py. Vérifie le fichier $LOG_FILE"
+    echo "Installation error of path.py. Check $LOG_FILE"
     exit 1
 fi
