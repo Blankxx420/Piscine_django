2630cd70a11a86cff5a5e2ddafe1c4d64854bf6d
Blankx <briceguichou.dev@gmail.com>
2026-07-20 23:30:46 +0200

    refactor: starting implementig LinkFinder Class instead of iterative version
    - rewrite of almost all of base code using class
    - refining better logic validation

--- Django-1-lib/ex03/roads_to_philosophy.py
+++ Django-1-lib/ex03/roads_to_philosophy.py
@@ -2,6 +2,35 @@ from sys import argv, exit, stderr
 import requests
 from bs4 import BeautifulSoup
 
+class LinkFinder:
+
+    def __init__(self):
+        self.depth = 0
+        self.namespaces = [
+            "Help:", "File:", "Wikipedia:", "Category:", "Talk:", "Special:",
+            "Portal:", "Template:", "Draft:", "User:", "MediaWiki:", "Module:",
+            "WT:", "WP:"
+        ]
+    
+    def is_href_valid(self, href):
+
+        if not href or not href.startwith("/wiki/"):
+            return False
+        
+        title_part= href[6:]
+        for ns in self.namespaces:
+            if title_part.startwith(ns):
+                return False
+        return True
+    
+    def is_italic(self, node):
+        current = node
+        while current and current.name != 'p':
+            if current.name in ["i", "em"]:
+                return True
+            current = current.parent
+
+
 def check_arguments():
     if len(argv) != 2:
         stderr.write("Error wrong number of arguments : Expected 1\n")
@@ -10,42 +39,8 @@ def check_arguments():
         stderr.write("Error invalid arguments : Expected a none empty string\n")
         exit(1)
 
-def construct_url(word):
-    search_word = word.replace(" ", "_")
-    final_url = f"https://en.wikipedia.org/wiki/{search_word}"
-    return final_url
 
-def get_wikipedia_page(search_word):
-    url = construct_url(search_word)
-    header = {
-        "User-Agent": "road_to_philosophy.py (your_email@example.com)"
-    }
-    try:
-        response = requests.get(url=url, headers=header)
-        response.raise_for_status()
-        return response.text
-    except requests.exceptions.RequestException as e:
-        stderr.write(f"Network or Server Error: {e}\n")
-        exit(1)
-
-def parse_response(response):
-    bs = BeautifulSoup(response, "html.parser")
-    id_bodycontent = bs.find(id="bodyContent")
-    paragraphes = id_bodycontent.find_all("p")
-    for p in paragraphes:
-        p_tag = p.find("a")
-        if p_tag is not None and p_tag.has_attr("title"):
-            return p.find("a")["title"]
-        0
-def iter_to_philosophy():
-    response = get_wikipedia_page(argv[1])
-    link = parse_response(response)
-    while link != "Philosophy":
-        print(link)
-        response = get_wikipedia_page(link)
-        link = parse_response(response)
             
 
 if __name__ == "__main__":
     check_arguments()
-    iter_to_philosophy()
