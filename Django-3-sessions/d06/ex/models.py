c18424de5513340d1a237d0c9df32c05fe710058
Blankx <briceguichou.dev@gmail.com>
2026-08-01 14:31:08 +0200

    feat : Enhancing model `ModelTips`
    - Adding to more field in model : `upvotes` & `downvotes` as ManytoMany relation with `User`
    - Impleting 2 property to get total upvote and total downvote

--- Django-3-sessions/d06/ex/models.py
+++ Django-3-sessions/d06/ex/models.py
@@ -1,6 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 class ModelTips(models.Model):
+    
     content = models.TextField(null=False)
     author = models.CharField(null=False)
     date_creation = models.DateTimeField(auto_now=True)
+
+    upvotes = models.ManyToManyField(User, related_name='upvoted_tips', blank=True)
+    downvotes = models.ManyToManyField(User, related_name='downvoted_tips', blank=True)
+
+
+    @property
+    def total_upvote(self):
+        return self.upvotes.count()
+
+    @property
+    def total_downvote(self):
+        return self.upvotes.count()
